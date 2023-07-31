129d49859f1bb39390659c589fca099faad1d081
garimasingh18 <garima.singh18@gmail.com>
2023-07-31 14:51:21 +0100

    passing values to pages

--- App.js
+++ App.js
@@ -1,85 +1,73 @@
 import React from "react";
-import { StyleSheet, View, Text, Pressable } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-//import { createStackNavigator } from "@react-navigation/stack";
+import ScreenA from "./screens/ScreenA";
+import ScreenB from "./screens/ScreenB";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-//HELP from - https://reactnavigation.org/docs/hello-react-navigation#creating-a-native-stack-navigator
 const Stack = createNativeStackNavigator();
-
-function ScreenA({ navigation }) {
-  const onPressHandler = () => {
-    navigation.navigate("Screen_B");
-  };
-
-  return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Screen A</Text>
-      <Pressable
-        onPress={onPressHandler}
-        style={({ pressed }) => ({
-          backgroundColor: pressed ? "#ddd" : "#0f0",
-        })}
-      >
-        <Text style={styles.text}>Go to Screen B</Text>
-      </Pressable>
-    </View>
-  );
-}
-
-function ScreenB({ navigation }) {
-  const onPressHandler = () => {
-    // navigation.navigate('Screen_A');
-    navigation.goBack();
-  };
-
-  return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Screen B</Text>
-      <Pressable
-        onPress={onPressHandler}
-        style={({ pressed }) => ({
-          backgroundColor: pressed ? "#ddd" : "#0f0",
-        })}
-      >
-        <Text style={styles.text}>Go Back to Screen A</Text>
-      </Pressable>
-    </View>
-  );
-}
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      // screenOptions={{
-      //   header: () => null
-      // }}
+        initialRouteName="Screen_A"
+        drawerPosition="left"
+        drawerType="front"
+        edgeWidth={100}
+        hideStatusBar={false}
+        overlayColor="#00000090"
+        drawerStyle={{
+          backgroundColor: "#e6e6e6",
+          width: 250,
+        }}
+        screenOptions={{
+          headerShown: true,
+          swipeEnabled: true,
+          gestureEnabled: true,
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#0080ff",
+          },
+          headerTintColor: "#ffffff",
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: "bold",
+          },
+        }}
       >
         <Stack.Screen
           name="Screen_A"
           component={ScreenA}
-          // options={{
-          //   header: () => null
-          // }}
+          options={{
+            title: "Screen_A Title",
+            drawerIcon: ({ focused }) => (
+              <FontAwesome5
+                name="autoprefixer"
+                size={focused ? 25 : 20}
+                color={focused ? "#0080ff" : "#999999"}
+              />
+            ),
+          }}
         />
-        <Stack.Screen name="Screen_B" component={ScreenB} />
+        <Stack.Screen
+          name="Screen_B"
+          component={ScreenB}
+          options={{
+            title: "Screen_B Title",
+            drawerIcon: ({ focused }) => (
+              <FontAwesome5
+                name="btc"
+                size={focused ? 25 : 20}
+                color={focused ? "#0080ff" : "#999999"}
+              />
+            ),
+          }}
+          initialParams={{ ItemName: "Item from Stack", ItemId: 12 }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 40,
-    fontWeight: "bold",
-    margin: 10,
-  },
-});
 
 export default App;
