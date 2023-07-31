5d818588254a92778a940a1265a62e1539bf5527
garimasingh18 <garima.singh18@gmail.com>
2023-07-31 13:30:58 +0100

    custom component

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import {
   TextInput,
   Pressable,
   Alert,
-  ToastAndroid,
   Modal,
   Image,
   ImageBackground,
 } from "react-native";
+import MyButton from "./src/CustomButton";
+import Header from "./src/Header";
 
 const App = () => {
   const [name, SetName] = useState("");
@@ -31,6 +32,7 @@ const App = () => {
         uri: "https://cdn.pixabay.com/photo/2013/07/12/12/35/texture-145968_960_720.png",
       }}
     >
+      <Header />
       <Modal
         visible={showWarning}
         transparent
@@ -64,17 +66,30 @@ const App = () => {
         placeholder="e.g. John"
         onChangeText={(value) => SetName(value)}
       />
-      <Pressable
+      <MyButton
+        onPressFunction={onPressHandler}
+        title={submitted ? "Clear" : "Submit"}
+        color={"#00ff00"}
+      />
+      <MyButton
+        onPressFunction={() => {}}
+        title={"Test"}
+        color={"#ff00ff"}
+        style={{ margin: 10 }}
+      />
+      {/* <Pressable
         onPress={onPressHandler}
         hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
-        android_ripple={{ color: "#00f" }}
+        android_ripple={{ color: '#00f' }}
         style={({ pressed }) => [
-          { backgroundColor: pressed ? "#dddddd" : "#00ff00" },
-          styles.button,
+          { backgroundColor: pressed ? '#dddddd' : '#00ff00' },
+          styles.button
         ]}
       >
-        <Text style={styles.text}>{submitted ? "Clear" : "Submit"}</Text>
-      </Pressable>
+        <Text style={styles.text}>
+          {submitted ? 'Clear' : 'Submit'}
+        </Text>
+      </Pressable> */}
       {submitted ? (
         <View style={styles.body}>
           <Text style={styles.text}>You are registered as {name}</Text>
